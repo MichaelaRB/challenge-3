68a20edadac7b77c53cb953b462d065d7dbf464a
Michaela Brown <michaelarbrown4@gmail.com>
2023-04-04 19:07:14 -0500

    finished populating the special characters array

--- Develop/script.js
+++ Develop/script.js
@@ -20,7 +20,7 @@ function generatePassword(v, w, x, y, z,)
   var lowerLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
   var upperLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
   var passNumbers = ["0","1","2","3","4","5","6","7","8","9"];
-  var specialChars = ["!","?","#","$","@"];
+  var specialChars = ["!","?","#","$","@","'",'"',"%","(",")","*","+",",","-","/",".",":",";","<",">","=","[","]","_","|","^","~","{","}"];
   var passwordChars = [];
   var passwordLength = v;
   var password = "";
